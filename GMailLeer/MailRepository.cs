262e7b2b47b60772db740b94a5bf727ca2ad613a
Ruben Marinez <me@rubenmarinez.com>
2024-12-17 09:33:12 -0600

    initial state

---
+++ GMailLeer/MailRepository.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using MailKit;
+using MailKit.Net.Imap;
+using MailKit.Search;
+
+namespace GMailLeer
+{
+    public class MailRepository
+    {
+        private readonly string mailServer, login, password;
+        private readonly int port;
+        private readonly bool ssl;
+
+        public MailRepository(string mailServer, int port, bool ssl, string login, string password)
+        {
+            this.mailServer = mailServer;
+            this.port = port;
+            this.ssl = ssl;
+            this.login = login;
+            this.password = password;
+        }
+
+        public IEnumerable<string> GetUnreadMails()
+        {
+            var count = 0;
+            var messages = new List<string>();
+
+            using (var client = new ImapClient())
+            {
+                client.Connect(mailServer, port, ssl);
+
+                // Note: since we don't have an OAuth2 token, disable
+                // the XOAUTH2 authentication mechanism.
+                client.AuthenticationMechanisms.Remove("XOAUTH2");
+
+                client.Authenticate(login, password);
+
+                // The Inbox folder is always available on all IMAP servers...
+                var inbox = client.Inbox;
+                inbox.Open(FolderAccess.ReadOnly);
+                var results = inbox.Search(SearchOptions.All, SearchQuery.Not(SearchQuery.Seen));
+                foreach (var uniqueId in results.UniqueIds)
+                {
+                    count++;
+                    var message = inbox.GetMessage(uniqueId);
+
+                    //if (message.Subject.ToString().Contains("ASAE"))
+                    //{
+                    //    messages.Add($">>> {count} [{message.From}] [{message.Subject}]");
+                    //}
+
+                    //messages.Add(message.HtmlBody);
+                    //messages.Add(message.Subject);
+                    //messages.Add(">>> [" + message.Subject + "]   " + message.TextBody);
+                    messages.Add($">>> {count} [{message.From}] [{message.Subject}]");
+
+                    //Mark message as read
+                    //inbox.AddFlags(uniqueId, MessageFlags.Seen, true);
+                }
+
+                client.Disconnect(true);
+            }
+
+            return messages;
+        }
+
+        public IEnumerable<string> GetAllMails()
+        {
+            var messages = new List<string>();
+
+            using (var client = new ImapClient())
+            {
+                client.Connect(mailServer, port, ssl);
+
+                // Note: since we don't have an OAuth2 token, disable
+                // the XOAUTH2 authentication mechanism.
+                client.AuthenticationMechanisms.Remove("XOAUTH2");
+
+                client.Authenticate(login, password);
+
+                // The Inbox folder is always available on all IMAP servers...
+                var inbox = client.Inbox;
+                inbox.Open(FolderAccess.ReadOnly);
+                var results = inbox.Search(SearchOptions.All, SearchQuery.NotSeen);
+                foreach (var uniqueId in results.UniqueIds)
+                {
+                    var message = inbox.GetMessage(uniqueId);
+
+                    messages.Add(message.HtmlBody);
+
+                    //Mark message as read
+                    //inbox.AddFlags(uniqueId, MessageFlags.Seen, true);
+                }
+
+                client.Disconnect(true);
+            }
+
+            return messages;
+        }
+    }
+}
---
+++ GMailLeer/Program.cs
@@ -0,0 +1,62 @@
+﻿//using System.Net.Http.Headers;
+//using System.Text;
+//using System.Xml;
+
+//try
+//{
+//    const string emailAddress = "rmf.asae.dev@gmail.com";
+//    // App Password, not password
+//    // See: https://support.google.com/accounts/answer/185833?hl=en
+//    const string appPassword = "tu password";
+
+//    string response;
+//    string title;
+//    string summary;
+//    XmlDocument xmlDocument = new XmlDocument();
+
+//    HttpClient httpClient = new HttpClient();
+
+//    // Logging in Gmail server to get data
+//    httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(Encoding.UTF8.GetBytes($"{emailAddress}:{appPassword}")));
+
+//    // Reading data and converting to string
+//    response = await httpClient.GetStringAsync(@"https://mail.google.com/mail/feed/atom");
+
+//    // Remove XML namespace to simplify parsing/selecting nodes
+//    response = response.Replace(@"<feed version=""0.3"" xmlns=""http://purl.org/atom/ns#"">", @"<feed>");
+
+//    // Loading into an XML so we can get information easily
+//    xmlDocument.LoadXml(response);
+
+//    // Amount of emails
+//    string nr = xmlDocument.SelectSingleNode(@"/feed/fullcount").InnerText;
+
+//    // Reading the title and the summary for every email
+//    foreach (XmlNode node in xmlDocument.SelectNodes(@"/feed/entry"))
+//    {
+//        title = node.SelectSingleNode("title").InnerText;
+//        summary = node.SelectSingleNode("summary").InnerText;
+
+//        Console.WriteLine($"> {title}");
+//        Console.WriteLine($"{summary}");
+//        Console.WriteLine();
+//    }
+//}
+//catch (HttpRequestException ex)
+//{
+//    Console.WriteLine($"Error al obtener los correos: {ex.Message}");
+//    Console.WriteLine($"Estado HTTP: {ex.StatusCode}");
+//}
+
+using GMailLeer;
+
+var mailRepository = new MailRepository("imap.gmail.com", 993, true, "tucorreo@gmail.com", "tupassword");
+//var allEmails = mailRepository.GetAllMails();
+var allEmails = mailRepository.GetUnreadMails();
+
+foreach (var email in allEmails)
+{
+    Console.WriteLine(email);
+}
+
+//Assert.IsTrue(allEmails.ToList().Any());
